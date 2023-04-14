accccb9afac51aad0a617945e706bde67e02f6ae
Mitra-Sadas <87385147+Mitra-Sadas@users.noreply.github.com>
2023-04-14 12:22:56 +0530

    markdown updated

--- Demo_Social_Insights_V1.py
+++ Demo_Social_Insights_V1.py
@@ -2329,7 +2329,7 @@ def func(choice):
 							st.markdown('**<p style="font-size:20px;border-radius:2%;text-align:center;">What are the key Topics discussed:question:</p>**',unsafe_allow_html=True)
 							plt,val = kic_graph(data)
 							st.pyplot(plt)
-							highlighted_text = f"<p style='color: black;''font-weight: bold';><span style='color: orange;'font-weight: bold';>{hcpname} </span> is discussing <span style='color: blue;'>{val.index[0]} </span>as a key area of focus with <span style='color: green;'>{round(val[0],2)}%</span> engagement rate.</p>"
+							highlighted_text = f"<p style='color: black;''font-weight: bold;'><span style='color: orange;'font-weight: bold;'>{hcpname} </span> is discussing <span style='color: blue;'>{val.index[0]} </span>as a key area of focus with <span style='color: green;'>{round(val[0],2)}%</span> engagement rate.</p>"
 							st.write(highlighted_text, unsafe_allow_html=True)
 							
 
